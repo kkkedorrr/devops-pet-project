bb38f7ae578be6277f77dabcf52e4fe41c5be497
artem-ko-MA <artem.ko@moonactive.com>
2025-12-04 13:49:27 +0200

    Add unit tests and multi-stage pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import unittest
+from app.app import app
+
+class BasicTests(unittest.TestCase):
+    def setUp(self):
+        # Create a test client
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_status_code(self):
+        # Send a GET request to /
+        result = self.app.get('/')
+        # Assert the status code is 200 (OK)
+        self.assertEqual(result.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
+    
